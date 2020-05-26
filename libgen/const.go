f9270b5d1bc69c3b31687d3675593887eeed1a05
Ryan Ciehanski <ryan@ciehanski.com>
2020-05-26 04:43:44 -0500

    move dbdumps regex to const

--- libgen/const.go
+++ libgen/const.go
@@ -22,6 +22,7 @@ const (
 	SearchMD5         = "[A-Z0-9]{32}"
 	booksdlReg        = "http://80.82.78.13/get\\.php\\?md5=\\w{32}\\&key=\\w{16}&mirr=1"
 	bokReg            = `\/dl\/\d{6}\/\w{6}`
+	dbdumpReg         = `(["])(.*?\.(rar|sql.gz))"`
 	bokDownloadLimit  = "WARNING: There are more than 5 downloads from your IP"
 	nineThreeReg      = `\/main\/\d{1}\/[A-Za-z0-9]{32}\/.+?(gz|pdf|rar|djvu|epub|chm)`
 	JSONQuery         = "id,title,author,filesize,extension,md5,year,language,pages,publisher,edition,coverurl"
